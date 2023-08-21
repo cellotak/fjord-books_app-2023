6805eaf8aa404d0fdc25d487c0e33d3d0ea63264
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-08-21 16:29:05 +0900

    routesを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    :registrations => 'users/registrations'
+  }
   resources :books
-  root "books#index"
+  root 'books#index'
 
-  # devise_scope :user do
-  #   get 'users/index', to 'users/registrations#index'
-  # end
+
+  devise_scope :user do
+    get "users/index", to: 'users/registrations#index'
+  end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
