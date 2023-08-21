0ff8e22b10fd1476d21d613a32733e4d17cf513d
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-08-21 16:17:32 +0900

    userモデルのコントローラを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :books
-  root "devise/sessions#new"
+  root "books#index"
+
+  # devise_scope :user do
+  #   get 'users/index', to 'users/registrations#index'
+  # end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
