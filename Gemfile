fcb467cc643fd0ecd7cf98482fd52e3eb75d493d
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-08-25 16:48:16 +0900

    letter_opener_webを導入、リンク貼って動線を整える

--- Gemfile
+++ Gemfile
@@ -70,6 +70,8 @@ group :development do
   gem 'i18n_generators'
   gem 'rubocop-fjord', require: false
   gem 'rubocop-rails', require: false
+
+  gem 'letter_opener_web'
 end
 
 group :test do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.order(:id).page(params[:page]).per(5)
+    @users = User.order(:id).page(params[:page]).per(3)
   end
 
 # GET /books/1 or /books/1.json
