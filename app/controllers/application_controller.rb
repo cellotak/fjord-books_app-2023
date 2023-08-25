48d67d0e1b827aa270073b7399f0b4780c79f7fb
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-08-25 19:11:33 +0900

    rubocopとerblintの指摘事項を修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :adress, :postcode, :introduction])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :adress, :postcode, :introduction])
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name adress postcode introduction])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name adress postcode introduction])
   end
 end
-
-
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   def index
     @users = User.order(:id).page(params[:page]).per(3)
   end
 
-# GET /books/1 or /books/1.json
+  # GET /books/1 or /books/1.json
   def show
     @user = User.find(params[:id])
   end
-
 end
