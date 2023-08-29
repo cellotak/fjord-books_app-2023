a350f2ae9bf2cb7bd37686f46ceeedc38b243b94
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-08-30 06:51:12 +0900

    アイコン画像のファイル形式のバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,18 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validate :validate_file_type
   has_one_attached :avatar
+
+  private
+
+  def validate_file_type
+    return unless avatar.attached?
+    errors.add(:avatar, I18n.t('errors.messages.file_type_not_image')) if !image?
+
+  end
+
+  def image?
+    %w[image/jpg image/jpeg image/gif image/png].include?(avatar.blob.content_type)
+  end
 end
