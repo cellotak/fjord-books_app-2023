c115f72f034d2d2dc8644ff019928b2a8317c007
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-09-05 16:06:37 +0900

    ページングのマジックナンバーを解消、seedsを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  ITEM_COUNT_PER_PAGE = 10
+
   def index
-    @users = User.order(:id).page(params[:page]).per(3)
+    @users = User.order(:id).page(params[:page]).per(ITEM_COUNT_PER_PAGE)
   end
 
   # GET /books/1 or /books/1.json
