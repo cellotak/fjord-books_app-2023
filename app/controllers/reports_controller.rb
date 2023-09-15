55f7b86aa39c2c2130a3cad3f180993ad4065259
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-09-15 16:01:38 +0900

    user_idをcurrent_user.idにする実装方法を変更

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -21,9 +21,7 @@ class ReportsController < ApplicationController
 
   # POST /reports or /reports.json
   def create
-    hoge = report_params
-    hoge["user_id"] = current_user.id
-    @report = Report.new(hoge)
+    @report = current_user.reports.build(report_params)
     respond_to do |format|
       if @report.save
         format.html { redirect_to report_url(@report), notice: "Report was successfully created." }
