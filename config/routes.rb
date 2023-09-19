4c372d92a15cee3688a38a7a0d3a4cdefe172a18
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-09-19 17:09:15 +0900

    commentモデルのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
-  resources :books
-  resources :reports
+  resources :books do
+    resorces :comments
+  end
+  resources :reports do
+    resources :comments
+  end
   resources :users, only: %i(index show)
 end
