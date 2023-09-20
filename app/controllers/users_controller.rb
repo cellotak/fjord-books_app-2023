9ef79051b202dc6c990254280183070fee2e205e
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-09-20 16:32:40 +0900

    エラーメッセージの追加及び軽微な修正
    current_user以外の日報に対してedit, update,
    destroyをしようとしたときにエラーメッセージを出すように変更
    
    rubocopの修正を何点か反映
    
    paramsとしてuser_idを許可しないように変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @reports = @user.reports
+    @reports = @user.reports.order(:id).page(params[:page])
   end
 end
