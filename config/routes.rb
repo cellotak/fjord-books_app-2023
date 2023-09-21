c18b536e7940a05aec5095e787d49e6b1eb1a18a
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-09-21 10:29:30 +0900

    routes.rbのミスを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
   resources :books do
-    resorces :comments
+    resources :comments
   end
   resources :reports do
     resources :comments
