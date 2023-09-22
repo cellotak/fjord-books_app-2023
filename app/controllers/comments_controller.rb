f4615e85a7b4516755a4c7a0e62f43c33525c38e
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-09-22 14:04:24 +0900

    reportに対するコメント機能を追加、showアクションとビューを作成
    reportのshow画面にも追加、commentのshow画面へのリンクが正しくない

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,9 @@
 class CommentsController < ApplicationController
-
+  before_action :set_commentable, only: %i[create show] 
+  
   def create
     @comment = @commentable.comments.build(comment_params)
-
+    @comment.user_id = current_user.id
     if @comment.save
       redirect_to [@commentable, @comment], notice: 'Comment was successfully created.'
     else
@@ -10,8 +11,18 @@ class CommentsController < ApplicationController
     end
   end
 
+  def show
+    @comment = @commentable.comments.find(params[:id])
+  end
+
+  private
+
   def comment_params
     params.require(:report).permit(:content)
+  end
+
+  def set_commentable
+    @commentable = Report.find(params[:report_id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'books#index'
   resources :books do
-    resources :comments, only: [:create], module: :books 
+    resources :comments, only: [:create, :show]
   end
   resources :reports do
-    resources :comments,only: [:create], module: :reports 
+    resources :comments,only: [:create, :show]
   end
   resources :users, only: %i(index show)
 end
