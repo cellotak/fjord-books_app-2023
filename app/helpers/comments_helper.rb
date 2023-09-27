9e3ee0ebe11c746954a872f8f126a10ed33efb7f
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-09-27 18:14:48 +0900

    URL生成を自作helperからpolymorphic_pathに変更

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,23 +1,6 @@
 # frozen_string_literal: true
 
 module CommentsHelper
-  def commentable_comment_path(comment)
-    case comment.commentable_type
-    when 'Report'
-      report_comment_path(comment.commentable, comment)
-    when 'Book'
-      book_comment_path(comment.commentable, comment)
-    end
-  end
-
-  def edit_commentable_comment_path(comment)
-    case comment.commentable_type
-    when 'Report'
-      edit_report_comment_path(comment.commentable, comment)
-    when 'Book'
-      edit_book_comment_path(comment.commentable, comment)
-    end
-  end
 
   def commentable_model_name(comment)
     case comment.commentable_type
