7ba7a14c3fa91f93921e98710ca6826efa713705
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-10-13 11:52:51 +0900

    reportモデルのテストに#editable？を追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,12 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#editable?" do
+    target_user = User.new
+    non_target_user = User.new
+    report = target_user.reports.new
+    
+    assert_equal true, report.editable?(target_user)
+    assert_equal false, report.editable?(non_target_user)
+  end
 end
