3683b986996ef8159bdd27a976aaaba29b6d9056
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-10-16 12:31:26 +0900

    #created_onテストをfixturesのデータを使用するように変更

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -7,17 +7,15 @@ class ReportTest < ActiveSupport::TestCase
     target_user = User.new
     non_target_user = User.new
     report = target_user.reports.new
-    
+
     assert_equal true, report.editable?(target_user)
     assert_equal false, report.editable?(non_target_user)
   end
 
   test "#created_on" do
     today = Date.today
-    user = User.create!(email:"bob@example.com", password:"password")
-    report = user.reports.create(title:"hoge", content:"hogehoge")  
+    @user = users(:alice)
+    report = @user.reports.create(title: 'test', content: 'This is test')
     assert_equal today, report.created_on
   end
-
-
 end
