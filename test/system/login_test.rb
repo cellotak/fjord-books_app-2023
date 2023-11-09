f782fc7d5240c6e8e9b7bc58743c79f724a0f5d1
cellotak <tak.2cellos.ashi.777@gmail.com>
2023-11-09 16:04:54 +0900

    systemテストの余分なsetupを削除

--- test/system/login_test.rb
+++ test/system/login_test.rb
@@ -3,10 +3,6 @@
 require 'application_system_test_case'
 
 class LoginTest < ApplicationSystemTestCase
-  setup do
-    @report = reports(:day1)
-  end
-
   test 'login' do
     visit root_url
     fill_in 'Eメール', with: 'alice@example.com'
